4d777ee868eaa4b596152fa6ef0fc19088baa7f9
Mayank Gupta <mayankopop001@gmail.com>
2026-06-02 15:44:57 +0530

    Week 1 Day 1 Solutions

---
+++ Week_1/Day_1/Twosum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+       int n=nums.size();
+      unordered_map<int,int> mpp;
+       for(int i=0;i<n;i++)
+       {
+        int x=target-nums[i];
+  if(mpp.find(x)!=mpp.end())
+  {
+    return {mpp[x],i};
+  }
+  mpp[nums[i]]=i;
+       }
+    return {};
+}
+};
